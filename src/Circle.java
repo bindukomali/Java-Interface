b98bcbf467efa0288253678ed8611c9421fcc0d1
bindukomali <yadlabindukomali12@gmail.com>
2021-02-18 19:26:06 +0530

    Commiting JDBC Code v1

---
+++ src/Circle.java
@@ -0,0 +1,36 @@
+public class Circle implements Shape {
+    
+    private double radius;
+
+
+ public Circle(double radius) {
+		super();
+		this.radius = radius;
+	}
+
+public void draw() {
+        
+        System.out.println("Drawing Circle");
+
+ }
+
+  public double getArea() {
+        
+        return Math.PI*this.radius*this.radius;
+    }
+    
+    public double getRadius()
+    {
+        return this.radius;
+    }
+}
+
+ 
+
+ 
+
+ 
+
+
+ 
+
---
+++ src/ShapeTest.java
@@ -0,0 +1,19 @@
+
+public class ShapeTest {
+	public static void main(String[]args) {
+		
+	//programming for interface not implementation
+	Shape shape = new Circle(10);
+	
+	Circle c=new Circle(10);
+	
+	shape.draw();
+	System.out.println("Radius ="+c.getRadius());
+	System.out.println("Area="+shape.getArea());
+	
+	//switching from one implementation to another easily
+	shape=new Rectangle(10,10);
+	shape.draw();
+	System.out.println("Area="+shape.getArea());
+	}
+}
